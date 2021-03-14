336604b3079f7f3f947c4efaa2a04678bd70461b
miriamleann <72045458+miriamleann@users.noreply.github.com>
2021-03-14 03:26:05 -0700

    project 2 progress

---
+++ des342_project_two/javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("button").onclick=function(){
+
+    document.getElementById("confirm").innerHTML="order placed. check email for confirmation";
+    document.getElementById("button").style.display="none";
+
+}
